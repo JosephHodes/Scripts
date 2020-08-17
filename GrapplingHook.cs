b4e0be09253554bcf287e29a8a9e187fa563aece
jojoho70 <jojoho70@gmail.com>
2020-08-17 01:27:21 -0400

    Update GrapplingHook.cs
    experimenting with grappling hooks

--- GrapplingHook.cs
+++ GrapplingHook.cs
@@ -1,3 +1,36 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class GrapplingHook : MonoBehaviour
+{
+    private InputSystem IS;
+    public SpringJoint springJoint;
+    public float FireDistance;
+    private CharacterController player;
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IS = FindObjectOfType<InputSystem>();
+        player = FindObjectOfType<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(IS.Fire))
+        {
+            GrappleFire();
+        }
+        if (Input.GetKeyUp(IS.Fire))
+        {
+            GrappleRelease();
+        }
+    }
     private void GrappleFire()
     {
 
