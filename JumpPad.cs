25e59e66c197b436e503327cb5f21fac2dbac469
jojoho70 <jojoho70@gmail.com>
2020-08-21 03:07:14 -0400

    Update JumpPad.cs

--- JumpPad.cs
+++ JumpPad.cs
@@ -1,22 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class JumpPad : MonoBehaviour
 {
-    private Rigidbody player;
     private GameObject playerboi;
-    public int launchHeight, launchforward;
-
+    public int launchHeight;
+    public int launchforward;
 
     private void Start()
     {
-        playerboi = FindObjectOfType<PlayerMovement>().gameObject;   
+       
     }
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)        
     {
-        
-        if (playerboi)
+        playerboi = FindObjectOfType<Rigidbody>().gameObject;
+        Debug.Log(other.gameObject.layer);
+        if (other.gameObject.layer == playerboi.layer) 
         {
             playerboi.GetComponent<Rigidbody>().AddForce(transform.up * launchHeight);
             playerboi.GetComponent<Rigidbody>().AddForce(transform.forward * launchforward) ;
