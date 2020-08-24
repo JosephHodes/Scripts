03da4ec5cf80e12ef36f95ccb81d22eb389dca2a
joseph <jojoho70@gmail.com>
2020-08-24 17:46:24 -0400

    made some scripts to make a randomized spawn point and spawning

--- JumpPad.cs
+++ JumpPad.cs
@@ -7,9 +7,16 @@ using UnityEngine;
 public class JumpPad : MonoBehaviour
 {
     private GameObject playerboi;
-    public int launchHeight;
+
     public int launchforward;
     public Array meme;
+
+    [Header("is this a jump pad")]
+    public bool jumpPad;
+    public int launchHeight;
+    [Header("is this a slowdown pad")]
+    public bool ReduceSpeedPad;
+    public int slowdown;
 
     private void Start()
     {
@@ -20,9 +27,11 @@ public class JumpPad : MonoBehaviour
 
         Debug.Log(collision.gameObject.layer);
         playerboi = collision.gameObject.GetComponent<PlayerMovement>().gameObject;
-        if (playerboi) { 
-            playerboi.GetComponent<Rigidbody>().AddForce(playerboi.transform.up * launchHeight);
-            playerboi.GetComponent<Rigidbody>().AddForce(playerboi.transform.forward * launchforward);
+        if (playerboi) {
+            if (jumpPad)
+            {
+                playerboi.GetComponent<Rigidbody>().AddForce(playerboi.transform.up * launchHeight);
+            }
 
            
         }
---
+++ RandomTableSpawn.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTableSpawn : MonoBehaviour
+{
+
+    private GameObject milk;
+    // Start is called before the first frame update
+    [SerializeField]
+    private List<MilkSpawnpoint> spawning;
+    public int randomNum;
+    public GameObject fridge;
+    private int amountoffridges;
+   
+
+    void Start()
+    {
+        foreach (MilkSpawnpoint milk in FindObjectsOfType<MilkSpawnpoint>())
+        {
+            if (!spawning.Contains(milk))
+            {
+                spawning.Add(milk);
+            }
+        }
+        InvokeRepeating("Spawnfridge",0.01f, 0.1f);
+
+    }
+    void Spawnfridge()
+    {
+        var norepeatingrannumbers = 0;
+        if (fridge == null)
+        {
+            {
+                if (norepeatingrannumbers == randomNum)
+                {
+                    randomNum = UnityEngine.Random.Range(0, spawning.Count);
+                }
+                else
+                {
+                    Instantiate(fridge, spawning[randomNum].transform);
+                    amountoffridges++;
+                    norepeatingrannumbers = randomNum;
+                }
+            }
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        randomNum = UnityEngine.Random.Range(0, spawning.Count);
+    }
+    
+}
